890aba5d0303eb4b123cb8d2deb7c923df20ad07
mhdfirassbarakat <firass.barakat1998@gmail.com>
2022-08-23 21:28:23 +0300

    new one

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+//css
+import "./App.css";
+//Components
+import Navbar from "./Components/Navbar/Navbar";
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
