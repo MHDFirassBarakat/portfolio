92ac067ea8fbe8e61e8c742bee441e2a4de696ef
mhdfirassbarakat <firass.barakat1998@gmail.com>
2022-09-08 21:41:03 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,23 @@
-import React, { useContext } from "react";
-//css
 import "./App.css";
-//Components
-import Navbar from "./Components/Navbar/Navbar";
-import Intro from "./Components/Intro/Intro";
-import Services from "./Components/Services/Services";
-import Experience from "./Components/Experience/Experience";
-import Works from "./Components/Works/Works";
-import Portfolio from "./Components/Portfolio/Portfolio";
-import Testimonials from "./Components/Testimonials/Testimonials";
-import Contact from "./Components/Contact/Contact";
-import Footer from "./Components/Footer/Footer";
-import { themeContext } from "./Context";
-const App = () => {
-  const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/NavBar/NavBar";
+import { Banner } from "./components/Banner/Banner";
+import { Skills } from "./components/Skills/Skills";
+import { Projects } from "./components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
+import { Footer } from "./components/Footer/Footer";
+
+function App() {
   return (
-    <div
-      className="App"
-      style={{
-        background: darkMode ? "black" : "",
-        color: darkMode ? "white" : "",
-      }}
-    >
-      <Navbar />
-      <Intro />
-      <Services />
-      <Experience />
-      <Works />
-      <Portfolio />
-      <Testimonials />
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
       <Contact />
       <Footer />
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,36 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "../MailchimpForm/MailchimpForm";
+import Logo from "../../assets/img/2.png";
+import navIcon1 from "../../assets/img/nav-icon1.svg";
+import navIcon2 from "../../assets/img/nav-icon2.svg";
+import navIcon3 from "../../assets/img/nav-icon3.svg";
+//css
+import "./Footer.css";
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={Logo} alt="Logo" id="logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/mhd-firass-barakat-51788b197/">
+                <img src={navIcon1} alt="Icon" />
+              </a>
+              <a href="https://www.facebook.com/firass.barakat.16">
+                <img src={navIcon2} alt="Icon" />
+              </a>
+              <a href="https://www.instagram.com/mhdfirassbarakat/">
+                <img src={navIcon3} alt="Icon" />
+              </a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved To TechCocktail</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
